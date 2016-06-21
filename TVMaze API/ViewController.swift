2a8174b5f7cc5c0ac351fbda609c3282edc21367
Fernando Cani <fernando.cani@outlook.com>
2016-06-21 06:01:15 -0300

    Initial Home TableView

--- TVMaze API/ViewController.swift
+++ TVMaze API/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  TVMaze API
 //
 //  Created by Fernando Cani on 6/21/16.
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let cellIdentifier = "mainCell"
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! MainTableViewCell
+        cell.lblTitle.text = "teste"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
 }
 
+class MainTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imgHeader:   UIImageView!
+    @IBOutlet weak var lblTitle:    UILabel!
+    
+}
